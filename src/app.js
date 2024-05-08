229054f176bce682b054eae0a536be6fb10de855
Styoks <113370103+Styoks@users.noreply.github.com>
2024-05-08 18:45:28 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,64 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronouns = ["The", "our"];
+let adj = ["great", "Last"];
+let nouns = ["Of", "puedes"];
+let domains = [".com", ".es", ".us"];
+
+let container = document.querySelector("#container");
+
+function domainGenerator(pronouns, adj, nouns, domains) {
+  for (let pronoun in pronouns) {
+    for (let adjective in adj) {
+      for (let noun in nouns)
+        for (let domain in domains) {
+          if (nouns[noun] == "puedes" && domains[domain] == ".es") {
+            container.innerHTML +=
+              pronouns[pronoun] +
+              adj[adjective] +
+              "pued" +
+              domains[domain] +
+              "</br>";
+            console.log(
+              pronouns[pronoun] + adj[adjective] + "pued" + domains[domain]
+            );
+          } else if (
+            pronouns[pronoun] == "The" &&
+            adj[adjective] == "Last" &&
+            nouns[noun] == "Of" &&
+            domains[domain] == ".us"
+          ) {
+            container.innerHTML +=
+              pronouns[pronoun] +
+              adj[adjective] +
+              nouns[noun] +
+              domains[domain] +
+              " " +
+              "Juegassssso" +
+              "</br>";
+            console.log(
+              pronouns[pronoun] +
+                adj[adjective] +
+                nouns[noun] +
+                domains[domain] +
+                " " +
+                "Juegassssso"
+            );
+          } else {
+            container.innerHTML +=
+              pronouns[pronoun] +
+              adj[adjective] +
+              nouns[noun] +
+              domains[domain] +
+              "</br>";
+            console.log(
+              pronouns[pronoun] + adj[adjective] + nouns[noun] + domains[domain]
+            );
+          }
+        }
+    }
+  }
+}
+
+domainGenerator(pronouns, adj, nouns, domains);
